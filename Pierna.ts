76719fa1871e176d2d1777059702e69ac30d2851
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-22 18:44:18 -0500

    Empezando el repositorio

---
+++ Pierna.ts
@@ -0,0 +1,18 @@
+const sentadilla = {
+	'6-2': '[10] 12 - 12 - 12 - 12',
+};
+const sentadillaIsometrica = {
+	'6-2': '[10]',
+};
+const sentadillaSalto = {
+	'6-2': '[10] 12 - 10 - 10 - 8',
+};
+const zancada = {
+	'6-2': '[10] 10 - 10 - 10 - ',
+};
+const zentadaIsometrica = {
+	'6-2': '[10]',
+};
+const maquina = {
+	'6-2': '[10]',
+};
